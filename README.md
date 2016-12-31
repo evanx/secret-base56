e5cd61b9781b8813bb70880fca7dfc1300d765c7
Evan Summers <evan.summers@gmail.com>
2016-12-31 14:12:59 +0000

    initial

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # random-base56
 
+Generate random token base56 which is alphanumeric upper and lower omitting letters 0 and I and digits zero and one
 
 ```javascript
 ```
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const crypto = require('crypto');
+const Letters24 = 'ABCDEFGHJKLMNPQRSTUVWXYZ';
+const Digits8 = '23456789';
+const Symbols = [Digits8, Letters24, Letters24.toLowerCase()].join('');
+assert.equal(Symbols.length, 56);
+const length = parseInt(process.env.length || '16');
+console.log(crypto.randomBytes(length).map(value => Symbols.charCodeAt(Math.floor(value*Symbols.length/256))).toString());
